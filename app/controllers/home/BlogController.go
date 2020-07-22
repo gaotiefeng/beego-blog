9cbe949d5b4a271e63c243a40903bc8b465f9794
gaotiefeng <1096392101@qq.com>
2020-07-22 10:08:00 +0800

    update blog.html

--- app/controllers/home/BlogController.go
+++ app/controllers/home/BlogController.go
@@ -6,12 +6,18 @@ type BlogController struct {
 	controllers.BaseController
 }
 
+/**
+
+ */
 func (this *BlogController) Index() {
 	this.Data["content"] = "welcome to blog"
 	this.Data["language"] = "go controller"
 	this.TplName = "index.html"
 }
 
+/**
+
+ */
 func (this *BlogController) Detail()  {
 
 	this.TplName = "blog.html"
